d0dbf766ed01a35dc81e5df856329f064939c9c9
Kanan Farzali <kanan.farzali@gmail.com>
2024-04-13 16:47:36 +0200

    refactoring folder structure

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,13 @@
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import { useEffect } from "react";
-import SearchBar from "../../components/layout/SearchBar";
-import Tasks from "../../components/tasks/Tasks";
-import AddBtn from "../../components/layout/AddBtn";
-import AddTaskModal from "../../components/tasks/AddTaskModal";
-import EditTaskModal from "../../components/tasks/EditTaskModal";
-import AddEmployeeModal from "../../components/employees/AddEmployeeModal";
-import EmployeeListModal from "../../components/employees/EmployeeListModal";
+import SearchBar from "@/components/layout/SearchBar";
+import Tasks from "@/components/tasks/Tasks";
+import AddBtn from "@/components/layout/AddBtn";
+import AddTaskModal from "@/components/tasks/AddTaskModal";
+import EditTaskModal from "@/components/tasks/EditTaskModal";
+import AddEmployeeModal from "@/components/employees/AddEmployeeModal";
+import EmployeeListModal from "@/components/employees/EmployeeListModal";
 
 const Home = () => {
   useEffect(() => {
